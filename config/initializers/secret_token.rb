3ef3ec113be8f5a93d63644cd90b8d0d60523876
Kevin English <kenglish@gmail.com>
2011-09-01 17:23:33 -0700

    Committing initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PnmClassCcScan::Application.config.secret_token = 'ab33968395e18340f48d31eff580958e1bdb7d9e6699a9f6402b58fec859693ea34594d3e3e928c1a1a8b38e991f9c60fbaffc51d4a39d9a8a3d35b41daf773f'
