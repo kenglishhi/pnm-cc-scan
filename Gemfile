fe801ed2935b673a9d461ff85be43aa8bd452784
Kevin English <kenglish@gmail.com>
2011-09-01 17:37:10 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+gem 'rails', '3.1.0'
+gem 'sqlite3'
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+gem 'jquery-rails'
+gem "haml", ">= 3.1.2"
+gem "haml-rails", ">= 0.3.4", :group => :development
+gem "rspec-rails", ">= 2.6.1", :group => [:development, :test]
+gem "factory_girl_rails", ">= 1.1.0", :group => :test
+gem "devise", ">= 1.4.4"
+gem "frontend-helpers"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PnmClassCcScan::Application.config.secret_token = '6c927ca2185e9145d1c6d2463ac80d25e78d023363932ac0441d44c9e7d39d51c567ef31cff7df2dcd412cf8001ecf25ebb3758006f06b47bfb88d1b71c38d89'
